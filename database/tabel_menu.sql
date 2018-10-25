efd73a8ff1f038eb282c570396764288a8bd13c8
Bakoy <rahmansyah.dany.dr@gmail.com>
2018-10-25 13:39:57 +0700

    database menu

---
+++ database/tabel_menu.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Okt 2018 pada 08.38
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sisurat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tabel_menu`
+--
+
+CREATE TABLE `tabel_menu` (
+  `id` int(11) NOT NULL,
+  `judul_menu` varchar(50) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  `is_main_menu` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tabel_menu`
+--
+
+INSERT INTO `tabel_menu` (`id`, `judul_menu`, `link`, `icon`, `is_main_menu`) VALUES
+(1, 'Dashboard', 'home', 'fa fa-dashboard', '0'),
+(2, 'Surat Masuk', '#', 'fa fa-envelope', '0'),
+(3, 'Surat Keluar', '#', 'fa fa-envelope', '0'),
+(4, 'Data Surat Masuk', 'Smasuk/data_table', 'fa-fa-dashboard', '2'),
+(5, 'Input Surat Masuk', 'Smasuk', 'fa-fa-dashboard', '2'),
+(6, 'Data Surat Keluar', 'Skeluar/data_table', 'fa-fa-dashboard', '3'),
+(7, 'Input Surat Keluar', 'Skeluar', '', '3'),
+(8, 'User', 'Login/user', 'fa fa-users', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tabel_menu`
+--
+ALTER TABLE `tabel_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tabel_menu`
+--
+ALTER TABLE `tabel_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
