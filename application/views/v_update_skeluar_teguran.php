23fd6e80a6840e014242c780f56bf9e389ba824e
hanimaliah <hanimaliah46@gmail.com>
2018-10-25 14:46:54 +0700

    tambh button kembali di update teguran

--- application/views/v_update_skeluar_teguran.php
+++ application/views/v_update_skeluar_teguran.php
@@ -73,6 +73,7 @@
                     </div>
                     <?php } } ?>
                 </div>
+                <a href="<?= base_url('Skeluar/data_table/') ?>" class="btn btn-primary"><i class="fa fa-mail-reply"></i> Kembali</a>
 
                 <button type="submit" class="pull-right btn btn-default" id="">Save
                 <i class="fa fa-arrow-circle-right"></i></button>
