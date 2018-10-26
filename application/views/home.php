2ec82987986729902ae8f7c8b2389e1867103e56
Bakoy <rahmansyah.dany.dr@gmail.com>
2018-10-26 09:01:44 +0700

    dany push:

--- application/views/home.php
+++ application/views/home.php
@@ -12,8 +12,8 @@
       <div class="row">
         <div class="col-lg-6 col-xs-9">
           <!-- small box -->
-          <div class="small-box bg-aqua">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-files-o"></i></span>
+          <div class="small-box bg-yellow">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
             <div class="inner">
               <h3><?php echo $datasmasuk ?></h3>
 
@@ -28,8 +28,8 @@
         <!-- ./col -->
         <div class="col-lg-6 col-xs-9">
           <!-- small box -->
-          <div class="small-box bg-green">
-            <span class="info-box-icon bg-green"><i class="fa fa-files-o"></i></span>
+          <div class="small-box bg-red">
+            <span class="info-box-icon bg-red"><i class="fa fa-files-o"></i></span>
             <div class="inner">
               <h3><?php echo $dataskeluar ?></h3>
 
